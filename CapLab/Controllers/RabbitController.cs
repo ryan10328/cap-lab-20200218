6d257e3dcbad9e4bc8645830ac73d3eadd5e7cc5
Ryan Tseng <ryan@wishingsoft.com>
2020-02-18 22:39:22 +0800

    Add project files.

---
+++ CapLab/Controllers/RabbitController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCore.CAP;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CapLab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RabbitController : ControllerBase
+    {
+
+        private readonly ICapPublisher _publisher;
+        private readonly ILogger<RabbitController> _logger;
+        public RabbitController(ICapPublisher publisher, ILogger<RabbitController> logger)
+        {
+            _publisher = publisher;
+            _logger = logger;
+        }
+
+        [HttpGet, Route("send-message")]
+        public async Task<IActionResult> SendMessageAsync()
+        {
+            var userId = Guid.NewGuid();
+            await _publisher.PublishAsync("ryan.exchange.direct", new { UserId = userId });
+            return Ok(userId);
+        }
+
+        [NonAction]
+        [CapSubscribe("ryan.exchange.direct", Group = "ryan.hello")]
+        public void MessageSubscriber(object payload)
+        {
+            var result = JsonConvert.SerializeObject(payload);
+            _logger.Log(LogLevel.Information, result);
+        }
+    }
+}
---
+++ CapLab/Startup.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CapLab
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCap(config =>
+            {
+                config.UseInMemoryStorage();
+                config.UseRabbitMQ(x =>
+                {
+                    x.HostName = "127.0.0.1";
+                    x.Port = 5672;
+                    x.UserName = "guest";
+                    x.Password = "guest";
+                });
+            });
+
+            services.AddControllers();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
